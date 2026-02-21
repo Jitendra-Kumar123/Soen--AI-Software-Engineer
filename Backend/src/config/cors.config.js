43a59cd0eed2756a2fc131a67925dbb3329b0b9b
Jitendra Kumar <jitendra0202006@gmail.com>
2026-02-21 23:39:44 +0530

    Add CORS config

---
+++ Backend/src/config/cors.config.js
@@ -0,0 +1,6 @@
+export const corsOptions = {
+    origin: process.env.CLIENT_URL || '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
+};
